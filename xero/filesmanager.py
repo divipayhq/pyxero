be7b587a1ed184591c21879baae4fef15aec54b8
Tao Zhang <taozhang@Taos-MacBook-Pro.local>
2022-12-13 13:06:26 +1030

    reorder import

--- xero/filesmanager.py
+++ xero/filesmanager.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 import os
 import requests
 from six.moves.urllib.parse import parse_qs
+
 from .auth import OAuth2Credentials
 from .constants import XERO_FILES_URL
 from .exceptions import (
